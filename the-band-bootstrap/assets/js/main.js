6072a9a4a9c745a9d0aaddc880818c6b8732f40b
N <nguyennguyenchi714@gmail.com>
2026-06-30 15:11:48 +0700

    nâng cấp giao diện

--- the-band-bootstrap/assets/js/main.js
+++ the-band-bootstrap/assets/js/main.js
@@ -16,6 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+    // Khởi tạo tất cả Bootstrap Tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
 
 
